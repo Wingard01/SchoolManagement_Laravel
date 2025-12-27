cb8409794981c76c136e97fc8833e8de12939c93
Simbulireki Wingard <simbulirekiwinga@gmail.com>
2025-12-27 23:26:33 +0300

    y

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+}); */
+use App\Http\Controllers\StudentController;
+
+Route::get('/', function () {
+    return redirect()->route('students.index');
 });
+
+Route::resource('students', StudentController::class);
+
